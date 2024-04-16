47930b1e7c83af6f3d58c012ebfbe355022454ba
kaziabulhasib <abulhasibkazi@gmail.com>
2024-04-16 14:56:44 +0530

    firebase Setup

--- src/Components/Estate/Estate.jsx
+++ src/Components/Estate/Estate.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const Estate = ({ estate }) => {
-  const { estate_title, image, location, id } = estate;
+  const { estate_title, image, location, id, segment_name } = estate;
   return (
     <div className='card card-compact w-96 bg-base-100 shadow-xl'>
       <figure>
@@ -9,7 +9,7 @@ const Estate = ({ estate }) => {
       </figure>
       <div className='card-body'>
         <h2 className='card-title'>{estate_title}</h2>
-        <p>{location}</p>
+        <p>{segment_name}</p>
         <div className='card-actions justify-end'>
           <Link to={`/estate/${id}`}>
             <button className='btn  bg-[#e8e5e5]'>View Details</button>
